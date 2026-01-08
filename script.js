e3ed0e6c781c5737c9f59431a8700ea79f405f92
azikiwefavour <favouromovighoazikiwe@gmail.com>
2026-01-08 22:57:13 +0100

    fix confirm password validation

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 console.log("Registration form!");
 
 function registerUser() {
-  //Registration logic goes here
+  // Full name
   let fullName = prompt("Enter your full name:");
-  //Check both names
   while (
     fullName
       .trim()
@@ -11,36 +10,45 @@ function registerUser() {
       .filter((word) => word !== "").length < 2
   ) {
     fullName = prompt(
-      "invalid name.please enter both your surname and your last name"
+      "Invalid name. Please enter both your first name and last name."
     );
-    return;
-  }
-  // registration logic goes here
-  let email = prompt("Enter your email:");
-  while (!email.includes("@") || !email.includes(".")) {
-    email = prompt("please enter a valid email address.");
-    return;
   }
 
-  //password
-  let password = prompt("create a password.your password must have atleast 8 character 1 number and 1 special character:");
-  while ((password.length < 8 || !/[!@#$%^&*()_+-=[\]{}|;:'"<>\/?]/.test(password) || !/[A-Z]/.test(password) || !/[0-9]/.test)) {
-    password = prompt("invalid password.your password must contain atleast 8 character 1 number and 1 special character:"); 
-    return; 
+  // Email
+  let email = prompt("Enter your email:");
+  while (!email.includes("@") || !email.includes(".")) {
+    email = prompt("Please enter a valid email address.");
   }
-    //confirmpassword
-  let confirmPassword =prompt("confirm password. password must match the password feild.");
-    while (confirmPassword !== password) {
-    confirmPassword = prompt("password must match.please enter password again.");
-    }
-    return;
+
+  // Password
+  let password = prompt(
+    "Create a password.\nMust be at least 8 characters, include 1 capital letter, 1 number, and 1 special character."
+  );
+
+  while (
+    password.length < 8 ||
+    !/[A-Z]/.test(password) ||
+    !/[0-9]/.test(password) ||
+    !/[!@#$%^&*()_+\-=[\]{}|;:'"<>\/?]/.test(password)
+  ) {
+    password = prompt(
+      "Invalid password.\nMust be at least 8 characters, include 1 capital letter, 1 number, and 1 special character."
+    );
   }
-  //Age
-  let Age = prompt("enter your age");
-  while (isNaN(Age) || Age <= 18){
-    Age = prompt ("invalid Age. please enter a valid Age.");
-    return;
+
+  // Confirm password
+  let confirmPassword = prompt("Confirm your password:");
+  while (confirmPassword !== password) {
+    confirmPassword = prompt("Passwords do not match. Please try again:");
   }
-  alert("Registration sucessful");
+
+  // Age
+  let age = prompt("Enter your age:");
+  while (isNaN(age) || age <= 18) {
+    age = prompt("Invalid age. You must be older than 18.");
+  }
+
+  alert("Registration successful!");
 }
+
 registerUser();
